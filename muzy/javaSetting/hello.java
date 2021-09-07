9ff52fffc60d3b8ae6ef11f8f5b8f4e2b558c393
muzy06038 <sub06038@naver.com>
2021-09-07 12:28:33 +0900

    file io

--- muzy/javaSetting/hello.java
+++ muzy/javaSetting/hello.java
@@ -1,7 +1,28 @@
 package muzy.javaSetting;
 
+import java.util.Scanner;
+import java.io.*;
+
 public class hello {
+    public static void Number(int j){
+        int i = j;
+        int a=0;
+        while(a<i){
+            System.out.println(a);
+            a++;
+        }
+    }
+
     public static void main(String[] args){
-        System.out.println("hello world");
+        try {
+            File file = new File("out.txt");
+            Scanner sc = new Scanner(file);
+            while(sc.hasNextInt()){
+                System.out.println(sc.nextInt()*1000);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
