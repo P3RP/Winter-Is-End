4706cf8cf502765669aa6a457f4101060afadc8c
muzy06038 <sub06038@naver.com>
2021-09-12 20:05:03 +0900

    spring boot start

--- muzy/week2/src/test/java/com/example/week2/Week2ApplicationTests.java
+++ muzy/week2/src/test/java/com/example/week2/Week2ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Week2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
