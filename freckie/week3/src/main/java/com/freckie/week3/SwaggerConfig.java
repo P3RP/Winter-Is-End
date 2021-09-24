89f0f2eb5ca65d94381ea20089fbe10df1ad5715
freckie <freckie@frec.kr>
2021-09-25 01:54:27 +0900

    Add: week3 project

---
+++ freckie/week3/src/main/java/com/freckie/week3/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.freckie.week3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * The Class SwaggerConfig.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+
+    }
+
+    private ApiInfo apiInfo() {
+        String description = "Winter-is-End";
+        return new ApiInfoBuilder()
+                .title("Winter-is-End Week3")
+                .description(description)
+                .version("0.0.1")
+                .build();
+    }
+
+}
